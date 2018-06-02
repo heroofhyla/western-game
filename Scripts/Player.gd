3a9e2049e72e47c6a39223c8dee7b94d6ca4b8ed
Aezart <heroofhyla@yahoo.com>
2018-06-02 15:13:59 -0700

    Punching, facing
    Add punching mechanic, and pots you can break by punching them.
    
    Also, enabled the player to face left or right (it's a little janky at the moment).

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,10 +6,23 @@ extends KinematicBody2D
 export var SPEED = 100
 export var JUMP_SPEED = 200
 export var GRAVITY = 300
+export var can_move = true
 var input = Vector2()
 var velocity = Vector2()
 var up = Vector2(0,-1)
 var on_floor = false
+var facing = 1
+
+var animations = {
+	"stand":{
+		-1: "StandLeft",
+		1: "StandRight"
+	},
+	"punch":{
+		-1:"PunchLeft",
+		1: "PunchRight"
+	}
+}
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -18,20 +31,35 @@ func _ready():
 func _physics_process(delta):
 	input.x = 0
 	input.y = 0
+	
 	if Input.is_action_pressed("move_left"):
+		
 		input.x -= 1
+		if on_floor and can_move:
+			facing = -1
+			$AnimationPlayer.play(animations["stand"][facing])
+		
 	if Input.is_action_pressed("move_right"):
 		input.x += 1
+		if on_floor and can_move:
+			facing = 1
+			$AnimationPlayer.play(animations["stand"][facing])
+	
 	if Input.is_action_just_pressed("jump"):
 		if on_floor:
 			velocity.y -= JUMP_SPEED
 			on_floor = false
-	
+
+	if Input.is_action_just_pressed("punch"):
+		if can_move:
+			can_move = false
+			$AnimationPlayer.play(animations["punch"][facing])
+			$AnimationPlayer.queue(animations["stand"][facing])
 	if Input.is_action_just_released("jump"):
 		if velocity.y < - 50:
 			velocity.y /= 2
+	
 	velocity.y += GRAVITY * delta
-
 	input = input.normalized() * SPEED
 	velocity.x = input.x
 	velocity = move_and_slide(velocity, up)
