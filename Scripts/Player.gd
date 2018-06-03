9a5bcf9bbdb714ba88108bbcde48902962302243
Aezart <heroofhyla@yahoo.com>
2018-06-02 20:21:13 -0700

    Animation Improvements
    Trying to figure out a good way to manage animations, without all the combinatorial explosion of punching while running, punching wall falling, etc. Some progress.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,13 +6,16 @@ extends KinematicBody2D
 export var SPEED = 100
 export var JUMP_SPEED = 200
 export var GRAVITY = 300
-export var can_move = true
 var input = Vector2()
 var velocity = Vector2()
 var up = Vector2(0,-1)
 var on_floor = false
 var facing = 1
-
+var state = "stand"
+var old_facing = 1
+var old_state = "stand"
+var next_state = null
+var i = 1
 var animations = {
 	"stand":{
 		-1: "StandLeft",
@@ -21,48 +24,94 @@ var animations = {
 	"punch":{
 		-1:"PunchLeft",
 		1: "PunchRight"
+	},
+	"run":{
+		-1:"RunLeft",
+		1:"RunRight"
+	},
+	"jump":{
+		-1:"JumpLeft",
+		1:"JumpRight"
 	}
 }
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
+func can_punch():
+	return (state == "stand"
+		or  state == "run"
+		or  state == "jump")
+
+func can_move():
+	return (state == "stand"
+	    or  state == "run"
+		or  state == "jump"
+		or  state == "punch" and not on_floor)
+
 func _physics_process(delta):
+	i += 1
+	i %= 100
 	input.x = 0
 	input.y = 0
-	
+	old_state = state
+	old_facing = facing
 	if Input.is_action_pressed("move_left"):
-		
-		input.x -= 1
-		if on_floor and can_move:
+		if can_move():
+			input.x -= 1
+
+		if on_floor and can_move():
+			state = "run"
 			facing = -1
-			$AnimationPlayer.play(animations["stand"][facing])
-		
+
 	if Input.is_action_pressed("move_right"):
-		input.x += 1
-		if on_floor and can_move:
+		if can_move():
+			input.x = 1
+
+		if on_floor and can_move():
+			state = "run"
 			facing = 1
-			$AnimationPlayer.play(animations["stand"][facing])
+
+	if input.x == 0 and state == "run":
+		state = "stand"
+
+	if on_floor and state == "jump":
+		state = "stand"
 	
 	if Input.is_action_just_pressed("jump"):
-		if on_floor:
+		if on_floor and can_move():
+			state = "jump"
 			velocity.y -= JUMP_SPEED
 			on_floor = false
 
 	if Input.is_action_just_pressed("punch"):
-		if can_move:
-			can_move = false
-			$AnimationPlayer.play(animations["punch"][facing])
-			$AnimationPlayer.queue(animations["stand"][facing])
+		if can_punch():
+			next_state = old_state
+			state = "punch"
+
 	if Input.is_action_just_released("jump"):
 		if velocity.y < - 50:
 			velocity.y /= 2
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		print(state + "=>" + str(next_state))
 	
 	velocity.y += GRAVITY * delta
 	input = input.normalized() * SPEED
 	velocity.x = input.x
 	velocity = move_and_slide(velocity, up)
+	
+	if $AnimationPlayer.current_animation != animations[state][facing] and next_state and state == old_state:
+		state = next_state
+		next_state = null
+
+	if state != old_state or facing != old_facing:
+		$AnimationPlayer.play(animations[state][facing])
+		if next_state:
+			$AnimationPlayer.queue(animations[next_state][facing])
+
 	
 	# Working around some weirdness with the built-in is_on_floor function.
 	if is_on_floor():
