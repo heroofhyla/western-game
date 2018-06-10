4094125ee3a663c8201a15f6ab9ee507c68266c6
Aezart <heroofhyla@yahoo.com>
2018-06-09 19:05:16 -0700

    Handle multiple autotiles

--- Utils/do_simple_autotile.rb
+++ Utils/do_simple_autotile.rb
@@ -6,6 +6,43 @@ require_relative "autotile"
 @min_x = @tile_hash.min_by{|cell|cell[:x]}[:x]
 @min_y = @tile_hash.min_by{|cell|cell[:y]}[:y]
 
+
+@tilesets = {
+  dirt:{
+    nw: 13,
+    w: 11,
+    c: 12,
+    n: 1,
+    ne: 6,
+    inw: 2,
+    ine: 5,
+    ise: 4,
+    isw: 3,
+    e: 7,
+    se: 8,
+    s: 9,
+    sw: 10  
+  },
+  brick:{
+    n: 14,
+    ne: 15,
+    inw: 16,
+    ine: 17,
+    ise: 18,
+    isw: 19,
+    nw: 20,
+    w: 21,
+    sw: 22,
+    s: 23,
+    se: 24,
+    e: 25,
+    c: 26
+  }
+}
+
+
 @tile_map = to_2d_array(@tile_hash)
-simple_auto!(@tile_map)
+@tilesets.values.each do |t|
+  simple_auto!(@tile_map, t)
+end
 puts to_tile_data(@tile_map, @min_x, @min_y)
